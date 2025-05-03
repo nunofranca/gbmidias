34e7ba8819256122c0e7ac5dabdc2d891d467cd9
Nuno França <contato@nunotech.com.br>
2025-05-03 19:57:18 -0300

    ..

--- app/Providers/RepositoryServiceProvider.php
+++ app/Providers/RepositoryServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Repositories\APIs\GPT\GptRepository;
+use App\Repositories\APIs\GPT\GptRepositoryInterface;
 use App\Repositories\APIs\WhatsApp\WhatsAppRepository;
 use App\Repositories\APIs\WhatsApp\WhatsRepositoryInterface;
 use App\Repositories\Ask\AskRepositoryEloquent;
@@ -24,6 +26,10 @@ class RepositoryServiceProvider extends ServiceProvider
         $this->app->bind(ClientRepositoryInterface::class, ClientRepositoryEloquent::class);
         $this->app->bind(AskRepositoryInterface::class, AskRepositoryEloquent::class);
         $this->app->bind(ResponseRepositoryInterface::class, ResponseRespositoryEloquent::class);
+
+
+        //gpt
+        $this->app->bind(GptRepositoryInterface::class, GptRepository::class)
 
 
     }
