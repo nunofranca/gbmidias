25b4af7e21f0d854d189fa5014dec962d0123f1b
Nuno França <contato@nunotech.com.br>
2025-05-04 12:57:59 -0300

    ..

--- app/Services/APIs/GPT/GptService.php
+++ app/Services/APIs/GPT/GptService.php
@@ -91,7 +91,7 @@ class GptService implements GptServiceInterface
     {
         $services = $this->serviceService->getByCategory(Str::lower($arguments['category']));
 
-        $this->gptRepository->runTool($client, $runStatus, $functionCall, $services->toArray());
+        $this->gptRepository->runTool($client, $runStatus, $functionCall, 'Estes sao os servicos disponiveis:'. $services->toArray());
   
         
     }
