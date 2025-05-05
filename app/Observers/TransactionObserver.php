62cea2b44724fd712cf47edba895df1ed6ec5a05
Nuno França <contato@nunotech.com.br>
2025-05-05 03:26:48 -0300

    ..

--- app/Observers/TransactionObserver.php
+++ app/Observers/TransactionObserver.php
@@ -28,7 +28,7 @@ class TransactionObserver
                        "action"=> "add"  ,                  
                        "service"=> $sale->services[0]->service,
                        "link" => $sale->link,
-                       "quantity"=> $sale->services->quantity
+                       "quantity"=> $sale->services[0]->quantity
                     ])->json();
             }
         }
