45f09e9365f147902812bf6dfddca0b4f78ad720
Nuno França <contato@nunotech.com.br>
2025-05-06 02:15:14 -0300

    .

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -27,8 +27,8 @@ class AppServiceProvider extends ServiceProvider
 
 
       Http::macro('whatsapp', function () {
-        return Http::withToken('EAAem1g2arPYBOyqQy041nNhkv34E0440CyOR9ANewYtFIdFcYTCZCAjG7wlLR7vuZCohcuKu1M72graD8rEd198it0ZCbnsNfp02JxXQO9POmx4ZC40nKbG5J9jYOWoQf2oArhJlEcL3e6gPHdPSQo5X4zAsWdfKcRe6ZC8MW0lb2S7e5NGxpCPCezvUjIcNP7ufKAwbDso424ZAlFhvTUHimJy0UZD')
-            ->baseUrl('https://graph.facebook.com/v22.0/551757028021017');
+        return Http::withToken('EAAem1g2arPYBO6ZBdZBZADdBaIiaPGSPAiZCPAjgQflJy0ENV8oXUoZC0J81ZBupQz5rAywm8nnNvgH0YaZCP3T9rnvoKgBYHKKzDdAL6JOxQDyZBQwPUSZCw5TvchyUxZAN60VfvIDtFPJoNhc7bSJxZATJf5wj98FJFcFByO8AA5d1ofkDXP0B1CEPJtc1ZCTDScHGaJQZACReFfRM7yW2XkUrNIQG4RkcZD')
+            ->baseUrl('https://graph.facebook.com/v22.0/649078624950335');
 
       });
 
