de5bc7b674efef587772797c169bcd0d2b12ac4b
Nuno França <contato@nunotech.com.br>
2025-11-13 15:24:55 -0300

    ..

--- app/Models/Tenant.php
+++ app/Models/Tenant.php
@@ -3,11 +3,14 @@ declare(strict_types=1);
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Attributes\ObservedBy;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
+
+#[ObservedBy(Tenant::class)]
 class Tenant extends Model
 {
     use  SoftDeletes;
