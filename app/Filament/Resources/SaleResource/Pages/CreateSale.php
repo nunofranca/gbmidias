6911c0658b69f8e463a297b9458340d6a2a129c9
Nuno França <contato@nunotech.com.br>
2025-11-22 14:11:13 -0300

    ..

--- app/Filament/Resources/SaleResource/Pages/CreateSale.php
+++ app/Filament/Resources/SaleResource/Pages/CreateSale.php
@@ -4,9 +4,19 @@ namespace App\Filament\Resources\SaleResource\Pages;
 
 use App\Filament\Resources\SaleResource;
 use Filament\Actions;
+use Filament\Actions\Action;
 use Filament\Resources\Pages\CreateRecord;
+use Illuminate\Support\Str;
 
 class CreateSale extends CreateRecord
 {
     protected static string $resource = SaleResource::class;
+    protected function getFormActions(): array
+    {
+        return [
+            Action::make('save')
+                ->label('Comprar') // ✅ muda o texto
+                ->submit('create'),
+        ];
+    }
 }
